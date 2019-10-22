57b74962b7c9091bebecd7bb7742e80ff99b5289
Harry Flanagan <S00189591@mail.itsligo.ie>
2019-10-22 12:56:31 +0100

    Add project files.

---
+++ SampleCA1/Player.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SampleCA1
+{
+    class Player
+    {
+        public int PlayerID { get; }
+        public string PlayerName { get; set; }
+        public  int PlayerScore { get; private set; } //can only be access from within the class
+
+        public Player(int id, string name, int score)
+        {
+            //sets properties within default constructor
+            PlayerID = id;
+            PlayerName = name;
+            PlayerScore = score;
+        }
+        public void IncreaseScore(int valueToAdd)
+        {
+            if (PlayerScore < 100)
+            {
+                PlayerScore += valueToAdd;
+            }
+        }
+
+        
+    }
+}
---
+++ SampleCA1/Program.cs
@@ -0,0 +1,66 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SampleCA1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Player player1 = new Player(1, "Millie", 0);
+            Player player2 = new Player(2, "Katie", 0);
+            Player player3 = new Player(3, "Kevin", 0);
+            Player player4 = new Player(4, "Conor", 0);
+            Player player5 = new Player(5, "Pauline", 0);
+
+            //List of players in player class
+            List<Player> allPlayers = new List<Player>();
+            //adds each player to the list
+            allPlayers.Add(player1);
+            allPlayers.Add(player2);
+            allPlayers.Add(player3);
+            allPlayers.Add(player4);
+            allPlayers.Add(player5);
+
+            //IncreasedScore based on method in Player Class
+            player1.IncreaseScore(1);
+            player2.IncreaseScore(10);
+
+            //display all players with scores based on Display method in Program.cs
+            Display(allPlayers);
+
+            int enteredID = 1;
+            do
+            {
+                Console.WriteLine();
+             Console.Write("Enter the player ID number you wish to update the score of or 0 to finish -->");
+             enteredID = (int.Parse(Console.ReadLine())-1);
+
+                Player selectedPlayer = allPlayers.ElementAt(enteredID);
+
+                selectedPlayer.IncreaseScore(1);
+
+                Display(allPlayers);
+
+            } while (enteredID !=-1);
+            
+            
+        }
+        private static void Display(List<Player> players)
+        {
+            Console.WriteLine("{0, -10}{1, -10}{2, -10}{3, -10}{4, -10}","Player1", "Player2", "Player3", "Player4", "Player5");
+
+            //loop through all players in list and display score for each
+            foreach (Player player in players)
+            {
+                Console.Write("{0,-10}",player.PlayerScore);
+                
+                
+            }
+        }
+
+    }
+}
